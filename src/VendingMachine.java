b853bd469fb4e3b00601ad3a5386dd80fc3412e6
hcort26 <144395316+hcort26@users.noreply.github.com>
2025-02-12 19:38:02 -0500

    pulling

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,13 +1,5 @@
 public class VendingMachine {
     public static void main(String[] args) throws Exception {
-    // Snacks:
-        double doritos = 1.25;
-        double chocolate = 1.00;
-        double sourpatchkids = 1.00;
-        double lays = 1.25;
-        double poptart = 1.50;
-        
-        
         
     }
 }
