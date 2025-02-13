71bd393449f4ab60568a066d8ac1f70a2b3bfbf8
reZdotjson <0083rei@proton.me>
2025-02-12 19:41:43 -0500

    Scanner added

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -7,10 +7,6 @@ public class VendingMachine {
         double lays = 1.25;
         double poptart = 1.50;
         
-        System.out.println("What would you like");
-        Scanner input = new Scanner(System.in);
-        int in = input.nextInt();
-
         
         
     }
