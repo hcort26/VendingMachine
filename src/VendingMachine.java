90e6ce56a7524c729dd881ceff23b64e805540fe
hcort26 <144395316+hcort26@users.noreply.github.com>
2025-02-12 19:21:45 -0500

    fixed snacks

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,20 +1,13 @@
 public class VendingMachine {
     public static void main(String[] args) throws Exception {
     // Snacks:
-        double doritos1 = 1.25;
-        double doritos2 = 1.25;
-        double doritos3 = 1.25;
+        double doritos1, doritos2, doritos3 = 1.25;
         double chocolate = 1.00;
         double sourpatchkids = 1.00;
-        double lays1 = 1.25;
-        double lays2 = 1.25;
-        double lays3 = 1.25;
-        double lays4 = 1.25;
-        double poptart1 = 1.50;
-        double poptart2 = 1.50;
-        double poptart3 = 1.50;
+        double lays1, lays2, lays3, lays4 = 1.25;
+        double poptart1, poptart2, poptart3 = 1.50;
         
-        
+
         
     }
 }
